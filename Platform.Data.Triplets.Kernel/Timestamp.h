cec771cfd75e28fbbd1668811ffd6a6a300fdd35
Константин Дьяченко <konard@me.com>
2019-08-22 09:04:12 +0300

    Initial code commit, forked from: https://github.com/Konard/LinksPlatform

---
+++ Platform.Data.Triplets.Kernel/Timestamp.h
@@ -0,0 +1,10 @@
+#ifndef __LINKS_TIMESTAMP_H__
+#define __LINKS_TIMESTAMP_H__
+
+// Модуль с функцией вычисления "штампа времени" (time-stamp), который гарантировано уникальный.
+
+#include "Common.h"
+
+signed_integer GetTimestamp();
+
+#endif
---
+++ Platform.Data.Triplets.Kernel/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+LD_LIBRARY_PATH=`pwd` ./test
