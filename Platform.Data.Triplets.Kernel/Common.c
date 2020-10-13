f7aa7563562f57e6f325615e101288c86f3b6cf2
Tynkute <aktivnygomes1@gmail.com>
2020-10-13 14:17:11 +0300

    Update Common.c

--- Platform.Data.Triplets.Kernel/Common.c
+++ Platform.Data.Triplets.Kernel/Common.c
@@ -1,24 +1,28 @@
 #include "Common.h"
 
-signed_integer Error(char* message)
-{
 #ifdef DEBUG
-    printf("%s\n\n", message);
+    signed_integer Error(char* message)
+    {
+        printf("%s\n\n", message);
+        return ERROR_RESULT;
+    }
+#else
+    #define Error(message) (ERROR_RESULT)
 #endif
-    return ERROR_RESULT;
-}
 
-signed_integer ErrorWithCode(char* message, signed_integer errorCode)
-{
 #ifdef DEBUG
-    printf("%s Error code: %" PRId64 ".\n\n", message, errorCode);
+    signed_integer ErrorWithCode(char* message, signed_integer errorCode)
+    {
+        printf("%s Error code: %" PRId64 ".\n\n", message, errorCode);
+        return SUCCESS_RESULT == errorCode ? ERROR_RESULT : errorCode;
+    }
+#else
+    #define ErrorWithCode(message, errorCode) (ERROR_RESULT)
 #endif
-    return SUCCESS_RESULT == errorCode ? ERROR_RESULT : errorCode;
-}
 
-void DebugInfo(char* message)
-{
 #ifdef DEBUG
-    printf("%s\n", message);
+    void DebugInfo(char* message)
+    {
+        printf("%s\n", message);
+    }
 #endif
-}
