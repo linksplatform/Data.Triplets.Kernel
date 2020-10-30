fe3fd2ea8e2b48ebcddc379208ed4ff16de05af0
Tynkute <aktivnygomes1@gmail.com>
2020-10-30 17:25:02 +0300

    Update test.c

--- Platform.Data.Triplets.Kernel/test.c
+++ Platform.Data.Triplets.Kernel/test.c
@@ -4,27 +4,27 @@
 
 int main() {
     
-//     printf("Startup.\n");
+    printf("Startup.\n");
     
-//     OpenLinks("db.links");
+    OpenLinks("db.links");
     
-//     printf("Database opened.\n");
+    printf("Database opened.\n");
     
-//     link_index isA = CreateLink(itself, itself, itself);
-//     link_index isNotA = CreateLink(itself, itself, isA);
-//     link_index link = CreateLink(itself, isA, itself);
-//     link_index thing = CreateLink(itself, isNotA, link);
+    link_index isA = CreateLink(itself, itself, itself);
+    link_index isNotA = CreateLink(itself, itself, isA);
+    link_index link = CreateLink(itself, isA, itself);
+    link_index thing = CreateLink(itself, isNotA, link);
     
-//     UpdateLink(isA, isA, isA, link); // После этого минимальное ядро системы можно считать сформированным
+    UpdateLink(isA, isA, isA, link); // После этого минимальное ядро системы можно считать сформированным
     
-//     DeleteLink(isA); // Одна эта операция удалит все 4 связи
-//     DeleteLink(thing);
+    DeleteLink(isA); // Одна эта операция удалит все 4 связи
+    DeleteLink(thing);
     
-//     printf("Test ok.\n");
+    printf("Test ok.\n");
     
-//     CloseLinks();
+    CloseLinks();
     
-//     printf("Database closed.\n");
+    printf("Database closed.\n");
     
     return 0;
 }
